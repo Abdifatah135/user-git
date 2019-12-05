aae8363c080040c66ba3b6f8a4c31b60d99c2848
Abdi <oxabdi75@gmail.com>
2019-12-05 21:38:11 +0300

    added function in profile servive ts

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders} from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  private clientid= "1c11515f882584fa245b";
+  private clientsecret = "9e72dfe575fbdaf3125ad65ee73aa6b210057b4d";
+
+  constructor(private http:HttpClient) { 
+     
+    this.username= "Abdifatah135"
+  }
 }
